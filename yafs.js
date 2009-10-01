f5994da5f7b6ccc5a3047103f6c8f80fb8fd558e
Gabriel Gilini <gabrielgilini@gmail.com>
2009-10-01 02:32:22 -0300

    First commit, but almost minimally functional. Maybe I'll trade mousemove listener for a setInterval'd function. It appears to perform better.

---
+++ yafs.js
@@ -0,0 +1,172 @@
+function YAFS(containerId)
+{
+    //var global = this;
+    var container = document.getElementById(containerId);
+    var scrollBar = getChildByProperty(container, 'className', 'yafs-scrollbar');
+    var handle = getChildByProperty(scrollBar, 'className', 'yafs-handle');
+    var content = getChildByProperty(container, 'className', 'yafs-content');
+    var containerHeight = container.offsetHeight;
+    var handleHeight = handle.offsetHeight;
+    var scrollBarHeight = scrollBar.offsetHeight - handleHeight;
+    var contentHeight = content.offsetHeight - containerHeight;
+    var mouseCoord;
+    var _evtsAdded = {};
+
+    handle.style.top = '0px';
+    content.style.top = '0px';
+
+    addEvent('mousedown', handle, startScrolling);
+    addEvent('mouseup', document, stopScrolling);
+
+    function startScrolling(evt)
+    {
+        mouseCoord = getMouseY(evt);
+
+        addEvent('mousemove', document, computeScroll);
+    }
+
+    function computeScroll(evt)
+    {
+        var hTop = window.parseInt(handle.style.top, 10);
+        var newCoord = getMouseY(evt);
+        hTop += (newCoord - mouseCoord);
+        mouseCoord = newCoord;
+        console.log(newCoord, mouseCoord);
+        handle.style.top = hTop + 'px';
+        scrollContent(hTop);
+    }
+
+    function stopScrolling()
+    {
+        removeEvent('mousemove', document, computeScroll);
+    }
+
+    function scrollContent(hTop)
+    {
+        content.style.top = (-(contentHeight * getScrolledPercentage(hTop))) + 'px';
+    }
+
+    function getScrolledPercentage(hTop)
+    {
+        return hTop / scrollBarHeight;
+    }
+
+    function getMouseY(evt)
+    {
+        return evt.pageY || evt.clientY +
+                document.body.scrollTop +
+                document.documentElement.scrollTop;
+    }
+
+    /**
+     * getChild searches for a childNode that matches the given property
+     * @param HTMLElement parentNode
+     * @param string propName The property that will be queried on the children nodes
+     * @param string propValue The value that will have to match
+     * @return HTMLElement|null the child element that matches the search
+     */
+    function getChildByProperty(parentNode, propName, propValue)
+    {
+        var children = parentNode.childNodes;
+        var target = null;
+        var propReg = new RegExp('(^|\\s+)'+propValue+'(\\s+|$)');
+
+        for(var i = 0, len = children.length; i < len; ++i)
+        {
+            if(children[i].nodeType == 1 && propReg.test(children[i][propName]))
+            {
+                target = children[i];
+                break;
+            }
+        }
+        children = null;
+        return target;
+    }
+
+    function addEvent(type, element, func, options)
+    {
+        options = options || {};
+        var cbFun = function(evt)
+        {
+            if(!evt)
+            {
+                evt = window.event;
+            }
+            if(typeof evt.target == 'undefined' && typeof evt.srcElement != 'undefined')
+            {
+                evt.target = evt.srcElement;
+            }
+            if(options.stopPropagation === true)
+            {
+                evt.cancelBubble = true;
+                if(typeof evt['stopPropagation'] != 'undefined')
+                {
+                    evt.stopPropagation();
+                }
+            }
+            if(options.preventDefault === true)
+            {
+                evt.returnValue = false;
+                if(dom.isHostMethod(evt, 'preventDefault'))
+                {
+                    evt.preventDefault();
+                }
+            }
+            return func(evt);
+        };
+        if (typeof func == 'function')
+        {
+            if (typeof element['addEventListener'] != 'undefined')
+            {
+                element.addEventListener(type, cbFun, false);
+            }
+            else if (typeof element['attachEvent'] != 'undefined')
+            {
+                element.attachEvent('on'+type, cbFun);
+            }
+            else
+            {
+                element['on' + type] = cbFun;
+            }
+
+            if(!_evtsAdded[type])
+            {
+                _evtsAdded[type] = [];
+            }
+            _evtsAdded[type].push({'cbFun': cbFun, 'origFun': func});
+        }
+    }
+
+    function removeEvent(type, element, func)
+    {
+        if (typeof func == 'function')
+        {
+            for(var i = 0, l = _evtsAdded[type].length; i < l; ++i)
+            {
+                if(func == _evtsAdded[type][i]['origFun'])
+                {
+                    func = _evtsAdded[type][i]['cbFun'];
+                    _evtsAdded[type].splice(i,1);
+                    break;
+                }
+            }
+            if (typeof element['removeEventListener'] != 'undefined')
+            {
+                element.removeEventListener(type, func, false);
+            }
+            else if (typeof element['detachEvent'] != 'undefined')
+            {
+                element.detachEvent('on' + type, func);
+            }
+            else
+            {
+                element['on' + type] = null;
+            }
+        }
+    }
+
+    return {
+        'getScrolledPercentage': getScrolledPercentage,
+        'scrollContent': scrollContent
+    };
+}
