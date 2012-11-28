2b4c38b28f564dd443d16fb5ad25836110d4d23e
Gabriel Gilini <gabriel.gilini@ppivot.com.br>
2012-11-28 16:16:27 -0200

    Lots of additions made by me and coworkers in the last 3 years

--- yafs.js
+++ yafs.js
@@ -19,39 +19,67 @@ if(API && API.attachDocumentReadyListener)
                         container = E(container);
                     }
                     var dragging = false;
+                    var mWheelAttached = false;
                     var orientation = options.horizontal?1:0;
-                    var scrollBar = E(container.query('.yafs-scrollbar').first());
-                    var handle = E(scrollBar.children(0));
-                    var content = E(container.query('.yafs-content').first());
+                    var content = typeof options.content == "undefined"
+                        ? E(container.query('.yafs-content').first())
+                        : (options.content instanceof E ? options.content : E(options.content));
+
+                    if(!options.skipScrollBars)
+                    {
+                        var scrollBar = typeof options.scrollbar == "undefined"
+                            ? E(container.query('.yafs-scrollbar').first())
+                            : (options.scrollbar instanceof E ? options.scrollbar : E(options.scrollbar));
+                        var handle = E(scrollBar.children(0));
+                        var handleSize = API.getElementSize(handle.element());
+                        var scrollBarSize = API.getElementSizeStyle(scrollBar.element());
+                        var scrollableSize = scrollBarSize[orientation] - handleSize[orientation];
+                        var scrollBarWrapper = typeof options.scrollbarwrapper == "undefined"
+                            ? scrollBar
+                            : (options.scrollbarwrapper instanceof E ? options.scrollbarwrapper : E(options.scrollbarwrapper));
+                    }
+
                     var containerSize = API.getElementSizeStyle(container.element());
                     var contentSize = API.getElementSize(content.element());
                     var scrollableContentSize = [contentSize[4] - containerSize[0], contentSize[5] - containerSize[1]];
-                    var handleSize = API.getElementSize(handle.element());
-                    var scrollBarSize = API.getElementSizeStyle(scrollBar.element());
-                    var scrollableSize = scrollBarSize[orientation] - handleSize[orientation];
+
                     var currentPos = 0;
 
+                    var wheelScrollPx = options.wheelScrollBy || 30;
+                    var mWheelListener = function (e, delta)
+                    {
+                        API.cancelPropagation(e);
+                        scrollBy(-delta * wheelScrollPx);
+                    };
+
                     var resizeHandle;
-                    if(E.prototype.size)
+                    if(E.prototype.size && handle && (typeof options.resizeHandle == 'undefined' || options.resizeHandle))
                     {
                         resizeHandle = function()
                         {
-                            handleSize[orientation] = containerSize[orientation] / contentSize[orientation] * containerSize[orientation];
+                            handleSize[orientation] = containerSize[orientation] / contentSize[orientation] * scrollBarSize[orientation];
                             if(handleSize[orientation] < 30)
                             {
                                 handleSize[orientation] = 30;
                             }
                             handle.size(orientation?null:handleSize[0], orientation?handleSize[1]:null);
+
+                            if(contentSize[orientation] <= containerSize[orientation] && scrollBar)
+                            {
+                                scrollBarWrapper.show(false, {'removeOnHide': options.removeOnHide});
+                            }
+                            else
+                            {
+                                scrollBarWrapper.show(true);
+                            }
+                            scrollableSize = scrollBarSize[orientation] - handleSize[orientation];
                         };
                     }
 
-                    if(contentSize[orientation] <= containerSize[orientation])
+
+                    if(handle)
                     {
-                        scrollBar.show(false, {'removeOnHide': options.removeOnHide});
-                    }
-                    else
-                    {
-                        handle.attachDrag(null,
+                         handle.attachDrag(null,
                             {
                                 'ondrag': function(c)
                                 {
@@ -71,35 +99,75 @@ if(API && API.attachDocumentReadyListener)
                                 ghost: 1
                             }
                         );
-                        resizeHandle && resizeHandle();
                         updateScrollableSizes();
                     }
 
                     function updateScrollableSizes()
                     {
+                        containerSize = API.getElementSizeStyle(container.element());
+                        contentSize = API.getElementSize(content.element());
+                        scrollableContentSize = [contentSize[4] - containerSize[0], contentSize[5] - containerSize[1]];
+
+                        handleSize = API.getElementSize(handle.element());
+                        scrollBarSize = API.getElementSizeStyle(scrollBar.element());
                         scrollableSize = scrollBarSize[orientation] - handleSize[orientation];
+
+                        if(resizeHandle)
+                        {
+                            resizeHandle();
+                        }
+                        if(scrollableContentSize[orientation] < currentPos && scrollTo)
+                        {
+                            scrollTo(scrollableContentSize[orientation], false);
+                        }
+
+                        if(contentSize[orientation] <= containerSize[orientation] && scrollBar)
+                        {
+                            scrollBarWrapper.show(false, {'removeOnHide': options.removeOnHide});
+                        }
+                        else
+                        {
+                            scrollBarWrapper.show(true);
+                        }
+
+                        if(scrollableContentSize[orientation] > 0)
+                        {
+                            if(!mWheelAttached && options.mouseWheel && API.attachMousewheelListener)
+                            {
+                                API.attachMousewheelListener(container.element(), mWheelListener);
+                                mWheelAttached = true;
+                            }
+                        }
+                        else
+                        {
+                            if(mWheelAttached && options.mouseWheel && API.detachMousewheelListener)
+                            {
+                                API.detachMousewheelListener(container.element());
+                                mWheelAttached = false;
+                            }
+                        }
                     }
 
                     var scrollTo, scrollBy, scrollContentTo, scrollHandleTo;
                     (function()
                     {
-                        var getOpts = function()
+                        var getOpts = function(fx)
                         {
-                            if(!dragging)
+                            if(!dragging && fx)
                             {
-                                return {duration: options.duration, ease: options.ease};
+                                return {duration: options.duration, ease: options.ease, fps: options.fps || 60};
                             }
                             return null;
                         };
 
                         var posFn = (options.horizontal
-                            ? function(el, px){el.position(null, px, getOpts());}
-                            : function(el, px){el.position(px, null, getOpts());}
+                            ? function(el, px, fx){el.position(null, px, getOpts(fx));}
+                            : function(el, px, fx){el.position(px, null, getOpts(fx));}
                         );
 
-                        scrollContentTo = function(px)
+                        scrollContentTo = function(px, effects)
                         {
-                            if(px < 0)
+                            if(px < 0 || scrollableContentSize[orientation] < 0)
                             {
                                 px = 0;
                             }
@@ -108,26 +176,41 @@ if(API && API.attachDocumentReadyListener)
                                 px = scrollableContentSize[orientation];
                             }
                             currentPos = px;
-                            posFn(content, -px);
+                            posFn(content, -px, effects);
+
+                            return px;
                         };
 
-                        scrollHandleTo = function(px)
+                        if(handle)
                         {
-                            if(px < 0)
+                            scrollHandleTo = function(px, effects)
                             {
-                                px = 0;
-                            }
-                            else if(px > scrollableSize)
-                            {
-                                px = scrollableSize;
-                            }
-                            posFn(handle, px);
-                        };
+                                if(px < 0 || scrollableSize <= 0)
+                                {
+                                    px = 0;
+                                }
+                                else if(px > scrollableSize)
+                                {
+                                    px = scrollableSize;
+                                }
+                                posFn(handle, px, effects);
+                            };
+                        }
 
-                        scrollTo = function(px)
+                        scrollTo = function(px, effects)
                         {
-                            scrollContentTo(px);
-                            scrollHandleTo((px / scrollableContentSize[orientation]) * scrollableSize);
+                            px = scrollContentTo(px, effects);
+                            if(scrollHandleTo)
+                            {
+                                if(scrollableContentSize[orientation] <= 0)
+                                {
+                                    scrollHandleTo(0, effects);
+                                }
+                                else
+                                {
+                                    scrollHandleTo((px / scrollableContentSize[orientation]) * scrollableSize, effects);
+                                }
+                            }
                         };
 
                         scrollBy = function(px)
@@ -141,7 +224,11 @@ if(API && API.attachDocumentReadyListener)
                     return {
                         'scrollTo': scrollTo,
                         'scrollBy': scrollBy,
-                        'getScrolledPx': function(){return currentPos;}
+                        'getScrolledPx': function(){return currentPos;},
+                        'updateScroll': function()
+                            {
+                                updateScrollableSizes();
+                            }
                     };
                 };
             }
